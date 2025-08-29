774af5e757d41a0061e97c9dfcec3fcd5d7a556f
Dano Day <danoday@Foxmail.com>
2025-08-29 16:03:03 +0800

    添加README

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TabBar from './views/TabBar.vue';
+</script>
+
+<template>
+  <TabBar />
+</template>
+
+<style scoped></style>
---
+++ src/views/TabBar.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>sghui</h1>
+</template>
